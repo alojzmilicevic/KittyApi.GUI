66475d7e472517509e721fd747f9b1cffcba95aa
Alojz Milicevic <alojz.milicev@gmail.com>
2022-12-07 02:10:18 +0100

    Changed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,27 @@ import svgrPlugin from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes("node_modules")) {
+            if (id.includes("@mui")) {
+              return "vendor_mui";
+            }
+
+            return "vendor"; // all other package goes here
+          }
+        },
+      },
+    },
+  },
+  resolve: {
+    alias: [
+      {
+        find: "./runtimeConfig",
+        replacement: "./runtimeConfig.browser",
+      },
+    ],
+  },
 });
