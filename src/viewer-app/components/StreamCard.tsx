6700c3243df720eed36ca8f02cb382eb47514502
Alojz Milicevic <alojz.milicevic@gmail.com>
2022-12-17 01:13:05 +0100

    Added fade in animation to card when it appears

--- src/viewer-app/components/StreamCard.tsx
+++ src/viewer-app/components/StreamCard.tsx
@@ -4,8 +4,9 @@ import {
     CardActionArea,
     CardContent,
     CardMedia,
-    styled,
-    Typography
+    Grow,
+    Typography,
+    styled
 } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import { resourcesUrl } from '../../authentication/service/authentication-service';
@@ -27,6 +28,7 @@ const StyledCardContent = styled(CardContent)({ display: 'flex' });
 
 const StreamCard = ({ stream, setShowStreams }: Props) => {
     const { streamTitle, streamerName, thumbnail, streamerUsername } = stream;
+
     const navigate = useNavigate();
 
     const handleClick = () => {
@@ -35,27 +37,29 @@ const StreamCard = ({ stream, setShowStreams }: Props) => {
     }
 
     return (
-        <Card>
-            <CardActionArea onClick={handleClick}>
-                <CardMedia
-                    component="img"
-                    height="140"
-                    image={`${resourcesUrl}${getImagePath(thumbnail.thumbnailPath)}`}
-                    alt={thumbnail.thumbnailName}
-                />
-                <StyledCardContent sx={{ display: 'flex' }}>
-                    <Avatar>{streamerName[0]}</Avatar>
-                    <StyledTextContent>
-                        <Typography variant="body2">
-                            {streamTitle}
-                        </Typography>
-                        <Typography variant="caption">
-                            {streamerName}
-                        </Typography>
-                    </StyledTextContent>
-                </StyledCardContent>
-            </CardActionArea>
-        </Card>
+        <Grow in>
+            <Card>
+                <CardActionArea onClick={handleClick}>
+                    <CardMedia
+                        component="img"
+                        height="140"
+                        image={`${resourcesUrl}${getImagePath(thumbnail.thumbnailPath)}`}
+                        alt={thumbnail.thumbnailName}
+                    />
+                    <StyledCardContent sx={{ display: 'flex' }}>
+                        <Avatar>{streamerName[0]}</Avatar>
+                        <StyledTextContent>
+                            <Typography variant="body2">
+                                {streamTitle}
+                            </Typography>
+                            <Typography variant="caption">
+                                {streamerName}
+                            </Typography>
+                        </StyledTextContent>
+                    </StyledCardContent>
+                </CardActionArea>
+            </Card>
+        </Grow>
     );
 };
 
